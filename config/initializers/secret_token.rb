63a020bd26577f005b63e2f3b39eb17c85d7611d
jacklin <jacklin10@gmail.com>
2012-07-28 12:49:27 -0500

    initial update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easygift::Application.config.secret_token = '3b29f2adcb9a3e167ea740ee6a24f940c3b99a3d2bc3ddc0a84fb3857b679cf89ff418dd5957d2ed511f470fa3ff07155f2298b6fb43691088a1fb83cbcf45d4'
